4cd640854f8a4e6fd27dee96a4d4122cdb09f84b
Beccari <marcelo.beccari@gmail.com>
2022-12-22 17:33:02 -0300

    - add crop capability - add react-image-crop dependency

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import ReactCrop from 'react-image-crop'
 import Tesseract from 'tesseract.js';
 import './App.css';
+import 'react-image-crop/dist/ReactCrop.css'
+
 
 function App() {
-  const [imagePath, setImagePath] = useState('');
+  const [image, setImage] = useState('');
   const [text, setText] = useState('');
+  const canvasRef = useRef(null);
+  const imageRef = useRef(null);
+  const [crop, setCrop] = useState(null);
+  const [completedCrop, setCompletedCrop] = useState(null);
 
   const handleChange = (event) => {
-    setImagePath(URL.createObjectURL(event.target.files[0]));
+    setImage(URL.createObjectURL(event.target.files[0]));
   }
 
+  const handleCompleteCrop = (c) => {
+    setCompletedCrop(c);
+    handleClick();
+  }
+  
   const handleClick = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+ 
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+
+    const cropX = crop.x;
+    const cropY = crop.y;
+
+    ctx.translate(-cropX, -cropY);
+
+    ctx.drawImage(imageRef.current, 0, 0);
+    const dataUrl = canvas.toDataURL("image/jpeg");
+
     Tesseract.recognize(
-      imagePath, "por+spa+eng",
+      dataUrl, "por+spa+eng",
       {
         //logger: m => console.log(m)
       }
@@ -22,11 +48,9 @@ function App() {
     })
     .then(result => {
       console.log("result", result);
-      let confidence = result.confidence;
-      console.log("confidence:", confidence);
-
       let text = result.data.text;
       setText(text);
+      navigator.clipboard.writeText(text);
     })
   }
 
@@ -38,15 +62,24 @@ function App() {
     <div className="App">
       <main className="App-main">
         <h3>Actual image uploaded</h3>
-        <img src={imagePath} className="App-logo" alt="logo"/>
-
-        <h3>Extracted text</h3>
-        <div className="text-box">
+        <ReactCrop crop={crop} onChange={c => setCrop(c)} onComplete={handleCompleteCrop}>
+          <img src={image} className="App-logo" alt="logo" ref={imageRef}/>
+        </ReactCrop>
+        <div style={{paddingTop:10}}>
+          <canvas 
+            ref={canvasRef}
+            style={{
+              border: '1px solid black',
+              width: completedCrop?.width ,
+              height: completedCrop?.height,
+            }}
+            />
+        </div>
+        <h3>Text will be copied to the clipboard!</h3>
+        <div className="pin-box">
           <p> {text} </p>
         </div>
         <input type="file" onChange={handleChange} />
-        <button onClick={handleClick} style={{height:50}}> convert to text</button>
-        <button onClick={handleCopyClick} style={{height:50}}> copy text to clipboard</button>
       </main>
     </div>
   );
